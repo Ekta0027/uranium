612b2604fb3602cd31179bf09bcca844723ee4e6
nikit <ektakumari0027@gmail.com>
2022-04-09 18:33:34 +0530

    change

--- src/routes/route.js
+++ src/routes/route.js
@@ -59,6 +59,84 @@ router.post('/test-post2', function (req, res) {
     console.log(data)
     res.send( {  msg: "hi guys..my 2nd post req"  }   )
 });
+let players =
+
+   [
+
+       {
+
+           "name": "manish",
+
+           "dob": "1/1/1995",
+
+           "gender": "male",
+
+           "city": "jalandhar",
+
+           "sports": [
+
+               "swimming"
+
+           ]
+
+       },
+
+       {
+
+           "name": "gopal",
+
+           "dob": "1/09/1995",
+
+           "gender": "male",
+
+           "city": "delhi",
+
+           "sports": [
+
+               "soccer"
+
+           ],
+
+       },
+
+       {
+
+           "name": "lokesh",
+
+           "dob": "1/1/1990",
+
+           "gender": "male",
+
+           "city": "mumbai",
+
+           "sports": [
+
+               "soccer"
+
+           ],
+
+       },
+
+   ]
+
+
+   router.post('/players', function (req, res) {
+       
+       //LOGIC WILL COME HERE
+         let x =req.body;
+         let playersName=x.name
+         for(let i=0; i<players.length; i++){
+             if(players[i].name == playersName){
+                 res.send("players Already Exists")
+             }
+         }
+         players.push(x);
+         res.send(players);
+
+
+    //    res.send(  { data: players , status: true }  )
+
+   })
 
 
 const randomController= require("../controllers/randomController.js")
